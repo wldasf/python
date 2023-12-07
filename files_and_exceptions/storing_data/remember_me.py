a99c342c4b2147bfdc214d525e3db4a6a03eaa22
wldasf <waleed.a.assaf@gmail.com>
2023-12-08 01:27:26 +0300

    update

--- files_and_exceptions/storing_data/remember_me.py
+++ files_and_exceptions/storing_data/remember_me.py
@@ -5,26 +5,33 @@ def get_stored_username(path):
     """Get stored username if available."""
     if path.exists():
         contents = path.read_text()
-        username = json.loads(contents)
-        return username
+        info = json.loads(contents)
+        return info
     else:
         return None
     
 def get_new_username(path):
     """Prompt for a new username."""
+    info = {}
     username = input("What is your name? ")
-    contents = json.dumps(username)
+    age = input("How old are you? ")
+    origin = input("Where are you from? ")
+    info['username_key'] = username
+    info['age_key'] = age
+    info['origin_key'] = origin
+    contents = json.dumps(info)
     path.write_text(contents)
-    return username
+    return info
 
 def greet_user():
     """Greet the user by name."""
     path = Path('username.json')
-    username = get_stored_username(path)
-    if username:
-        print(f"Welcome back, {username}!")
+    info = get_stored_username(path)
+    if info:
+        print(f"Your name is {info['username_key']} you are {info['age_key']} from {info['origin_key']}.")
+        print(f"Welcome back, {info['username_key']}!")
     else:
-        username = get_new_username(path)
-        print(f"We'll remember you when you come back, {username}!")
+        info = get_new_username(path)
+        print(f"We'll remember you when you come back, {info['username_key']}!")
 
 greet_user()
