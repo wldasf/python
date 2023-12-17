851114b5f22b6160d5d163d2ba56920c61547468
wldasf <assafw25@gmail.com>
2023-12-17 13:09:52 +0300

    update

--- testing_code/city_country/city_functions.py
+++ testing_code/city_country/city_functions.py
@@ -1,3 +1,6 @@
-def city_country(city, country, population):
-    s = f"{city}, {country} - population {population}"
-    return  s.title() 
+def city_country(city, country, population=0):
+    if population > 0:
+        s = f"{city}, {country} - population {population}"
+    else:
+        s = f"{city}, {country}"
+    return  s.title()
--- testing_code/city_country/test_cities.py
+++ testing_code/city_country/test_cities.py
@@ -1,5 +1,9 @@
 from city_functions import city_country
 
 def test_city_country():
-    s = city_country('santiago', 'chile') 
+    s = city_country('santiago', 'chile')
     assert s == 'Santiago, Chile'
+
+def test_city_country_population():
+    x = city_country('santiago', 'chile', 5000000)
+    assert x == 'Santiago, Chile - Population 5000000'
