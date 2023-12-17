6cea0794b044791ac654a5b1b0ef5d025f142fae
wldasf <assafw25@gmail.com>
2023-12-17 16:28:54 +0300

    update

--- testing_code/test_survey.py
+++ testing_code/test_survey.py
@@ -1,13 +1,21 @@
+import pytest
 from survey import AnonymousSurvey
 
-def test_store_single_response():
+@pytest.fixture
+def language_survey():
+    """A survey that will be available to all test functions."""
+    question = "What language did you first learn to speak?"
+    language_survey = AnonymousSurvey(question)
+    return language_survey
+
+def test_store_single_response(language_survey):
     """Test that a single response is stored properly."""
     question = "What language did you first learn to speak?"
     language_survey = AnonymousSurvey(question)
     language_survey.store_response('English')
     assert 'English' in language_survey.responses
 
-def test_store_three_responses():
+def test_store_three_responses(language_survey):
     """Test that three individual responses are stored properly."""
     question = "What language did you first learn to speak?"
     language_survey = AnonymousSurvey(question)
