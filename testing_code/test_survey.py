a680efe27d60d3b3eba6905cf62a198c254cc819
wldasf <waleed.a.assaf@gmail.com>
2023-12-27 15:47:11 +0300

    update

--- testing_code/test_survey.py
+++ testing_code/test_survey.py
@@ -10,15 +10,11 @@ def language_survey():
 
 def test_store_single_response(language_survey):
     """Test that a single response is stored properly."""
-    question = "What language did you first learn to speak?"
-    language_survey = AnonymousSurvey(question)
     language_survey.store_response('English')
     assert 'English' in language_survey.responses
 
 def test_store_three_responses(language_survey):
     """Test that three individual responses are stored properly."""
-    question = "What language did you first learn to speak?"
-    language_survey = AnonymousSurvey(question)
     responses = ['English', 'Spanish', 'Mandarin']
     for response in responses:
         language_survey.store_response(response)
