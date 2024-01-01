f9cab2a6eac25bd27f9f85f80302a5108023d85f
wldasf <waleed.a.assaf@gmail.com>
2024-01-01 21:44:04 +0300

    exercise complete

--- testing_code/test_employee.py
+++ testing_code/test_employee.py
@@ -1,11 +1,14 @@
+import pytest
 from employee import Employee
 
-def test_give_default_raise():
-    emp = Employee('waleed', 'assaf', 5000)
-    emp.give_raise()
-    assert emp.annual_salary == 10000
+@pytest.fixture
+def employee_salary():
+    return Employee('waleed', 'assaf', 5000)
 
-def test_give_custom_raise():
-    emp = Employee('waleed', 'assaf', 7000)
-    emp.give_raise(7000)
-    assert emp.annual_salary == 14000
+def test_give_default_raise(employee_salary):
+    employee_salary.give_raise()
+    assert employee_salary.annual_salary == 10000
+
+def test_give_custom_raise(employee_salary):
+    employee_salary.give_raise(7000)
+    assert employee_salary.annual_salary == 12000
