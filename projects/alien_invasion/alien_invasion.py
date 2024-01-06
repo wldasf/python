cc97f409005d8940d6a2e854215f4c917e596dea
wldasf <waleed.a.assaf@gmail.com>
2024-01-06 18:31:10 +0300

    unused variable

--- projects/alien_invasion/alien_invasion.py
+++ projects/alien_invasion/alien_invasion.py
@@ -17,9 +17,6 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        # Set the background color.
-        self.bg_color = (230, 230, 230)
-
     def run_game(self):
         """Start the main loop for the game."""
         while True:
