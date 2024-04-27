bb5549865690373b5856bd1054a8172fdbbcff38
wldasf <waleed.a.assaf@gmail.com>
2024-04-27 20:36:46 +0300

    ship moves to right

--- projects/alien_invasion/alien_invasion.py
+++ projects/alien_invasion/alien_invasion.py
@@ -26,12 +26,16 @@ class AlienInvasion:
             self._check_events()
             self._update_screen()
             self.clock.tick(60)
-    
+
     def _check_events(self):
         """Respond to keypresses and mouse events."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right.
+                    self.ship.rect.x += 1
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
